3bc29320e81df451f0cd1a350516c928868611be
salavat <asfanov@gmail.com>
2022-12-08 22:50:36 +0500

    feat: configuration webpack & eslint under ts

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '3fc310bbb0044d928a0422c620b1b021', // получите свой ключ https://newsapi.org/
         });
     }
 }
